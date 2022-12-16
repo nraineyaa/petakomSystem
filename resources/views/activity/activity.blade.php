20331a66b32975f31e01d0f6239253a771f77269
apikolll <afiqnrzm@hotmail.com>
2022-12-17 03:44:39 +0800

    test pull request

--- resources/views/activity/activity.blade.php
+++ resources/views/activity/activity.blade.php
@@ -7,6 +7,7 @@
     <section class="p-5">
         <div class="container">
             <h1>Hello, this is activity page</h1>
+            <p class="lead">What a beautiful </p>
         </div>
     </section>
     
