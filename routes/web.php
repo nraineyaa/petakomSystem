deaa95204b7aaab9ddca7c96e1f46d1574beb7fa
cainpav <nurnadhirah2803@gmail.com>
2022-12-27 16:49:17 +0800

    delete ==

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-=======
 
 Route::get('/testactivity', function(){
     return view('activity.activity');
