1f85f047f7797604dba72d5ccfb241c625e9c331
apikolll <afiqnrzm@hotmail.com>
2023-01-14 12:25:56 +0800

    .

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -26,7 +26,7 @@
             <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
                 <ul class="navbar-nav mb-2 mb-lg-0 text-center">
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="/homepage">Home</a>
+                        <a class="nav-link active" aria-current="page" href="/">Home</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="{{ route('proposed.activity') }}">Activities</a>
