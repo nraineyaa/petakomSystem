f9b5d71294cbb42c00c4234e4c0d7abd7b00728f
nraineyaa <98253793+nraineyaa@users.noreply.github.com>
2023-01-26 18:05:47 +0800

    Update updateList.blade.php

--- resources/views/register/updateList.blade.php
+++ resources/views/register/updateList.blade.php
@@ -28,21 +28,6 @@
                                             </div>
                                         </div>
 
-
-                                        <br>
-
-                                        <div class="row">
-                                            <div class="form-group col-md-6">
-                                                <label for="password">{{ __('Password') }}</label>
-                                                <input id="password" type="password" class="form-control" name="password" value="{{$register->password}}" readonly>
-                                            </div>
-
-                                            <div class="form-group col-md-6">
-                                                <label for="confirmPass">{{ __('Confirm Password') }}</label>
-                                                <input id="confirmPass" type="password" class="form-control" name="confirmPass" value="{{$register->confirmPass}}" readonly>
-                                            </div>
-                                        </div>
-
                                         <br>
 
 
