34727c7ee845e0e31da610ad05862b7e7557f779
nraineyaa <98253793+nraineyaa@users.noreply.github.com>
2023-04-03 22:09:50 +0800

    last commit

---
+++ database/migrations/2023_01_26_124106_create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+
+            $table->id();
+            $table->bigInteger('user_id');
+            $table->string('email');
+            $table->string('password');
+            $table->string('confirmPass');
+            $table->string('Fname');
+            $table->double('Lname');
+            $table->string('phoneNum');
+            $table->string('category');
+            $table->string('address');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+};
